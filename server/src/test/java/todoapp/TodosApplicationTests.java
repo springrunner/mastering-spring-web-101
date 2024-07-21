a008a3b6f5c171a7069485148684a31265275125
Arawn Park <arawn.kr@gmail.com>
2024-07-21 23:48:59 +0900

    feat(server): Create server project in spring-initializr

---
+++ server/src/test/java/todoapp/TodosApplicationTests.java
@@ -0,0 +1,13 @@
+package todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
