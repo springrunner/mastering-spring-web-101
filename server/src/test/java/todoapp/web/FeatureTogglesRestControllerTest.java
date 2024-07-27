ff812acdd5ae1af916a9d3e47e5af20df238d76c
Arawn Park <arawn.kr@gmail.com>
2024-07-27 20:22:51 +0900

    feat(server): feat(server): Implement request mapping for feature-toggles

---
+++ server/src/test/java/todoapp/web/FeatureTogglesRestControllerTest.java
@@ -0,0 +1,33 @@
+package todoapp.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import todoapp.web.model.FeatureTogglesProperties;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FeatureTogglesRestController.class)
+class FeatureTogglesRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FeatureTogglesProperties featureTogglesProperties;
+
+    @Test
+    void featureToggles() throws Exception {
+        when(featureTogglesProperties.auth()).thenReturn(true);
+        when(featureTogglesProperties.onlineUsersCounter()).thenReturn(false);
+
+        mockMvc.perform(get("/api/feature-toggles"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"auth\":true,\"onlineUsersCounter\":false}"));
+    }
+}
