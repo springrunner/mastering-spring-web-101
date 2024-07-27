2fbe71cf2fca89b1b35560229550a225001db35c
Arawn Park <arawn.kr@gmail.com>
2024-07-27 21:05:17 +0900

    feat(server): Use server-sent events in user-counter

--- server/src/main/java/todoapp/web/OnlineUsersCounterController.java
+++ server/src/main/java/todoapp/web/OnlineUsersCounterController.java
@@ -3,9 +3,8 @@ package todoapp.web;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import todoapp.web.support.ConnectedClientCountBroadcaster;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * 실시간 사이트 접속 사용자 수 카운터 컨트롤러이다.
@@ -21,17 +20,8 @@ public class OnlineUsersCounterController {
      * HTML5 Server-sent events(https://en.wikipedia.org/wiki/Server-sent_events) 명세를 구현했다.
      */
     @RequestMapping(path = "/stream/online-users-counter", produces = "text/event-stream")
-    public void counter(HttpServletResponse response) throws Exception {
-        response.setCharacterEncoding("utf-8");
-        response.setContentType("text/event-stream");
-
-        for (int number = 1; number < 11; number++) {
-            TimeUnit.SECONDS.sleep(1);
-
-            var outputStream = response.getOutputStream();
-            outputStream.write(("data: " + number + "\n\n").getBytes());
-            outputStream.flush();
-        }
+    public SseEmitter counter(HttpServletResponse response) throws Exception {
+        return broadcaster.subscribe();
     }
 
 }
