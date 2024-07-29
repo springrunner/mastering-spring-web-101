3a9f9115b41c42e7164d657e20ea21685ec7e719
Arawn Park <arawn.kr@gmail.com>
2024-07-29 19:58:42 +0900

    feat(server): Use authorization to user profile endpoint

--- server/src/main/java/todoapp/web/UserRestController.java
+++ server/src/main/java/todoapp/web/UserRestController.java
@@ -1,13 +1,10 @@
 package todoapp.web;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import todoapp.security.UserSession;
 import todoapp.web.model.UserProfile;
-
-import java.util.Objects;
 
 /**
  * @author springrunner.kr@gmail.com
@@ -15,12 +12,10 @@ import java.util.Objects;
 @RestController
 public class UserRestController {
 
+    @RolesAllowed(UserSession.ROLE_USER)
     @GetMapping("/api/user/profile")
-    public ResponseEntity<UserProfile> userProfile(UserSession userSession) {
-        if (Objects.nonNull(userSession)) {
-            return ResponseEntity.ok(new UserProfile(userSession.getUser()));
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    public UserProfile userProfile(UserSession userSession) {
+        return new UserProfile(userSession.getUser());
     }
 
 }
