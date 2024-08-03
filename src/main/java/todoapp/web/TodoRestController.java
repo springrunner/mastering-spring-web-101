21fd1e63ac7a06400b5b1579b2fbe8dee2410b43
Arawn Park <arawn.kr@gmail.com>
2024-08-03 18:05:55 +0900

    /api/todos 엔드포인트를 매핑하고 할일 등록 기능을 구현한다

--- src/main/java/todoapp/web/TodoRestController.java
+++ src/main/java/todoapp/web/TodoRestController.java
@@ -1,24 +1,43 @@
 package todoapp.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import todoapp.core.todo.application.TodoFind;
+import todoapp.core.todo.application.TodoRegistry;
 import todoapp.core.todo.domain.Todo;
 
 import java.util.Objects;
 
 @RestController
+@RequestMapping("/api/todos")
 public class TodoRestController {
 
     private final TodoFind find;
+    private final TodoRegistry registry;
 
-    public TodoRestController(TodoFind find) {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    public TodoRestController(TodoFind find, TodoRegistry registry) {
         this.find = Objects.requireNonNull(find);
+        this.registry = Objects.requireNonNull(registry);
     }
 
-    @RequestMapping("/api/todos")
+    @GetMapping
     public Iterable<Todo> readAll() {
         return find.all();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody CreateTodoCommand command) {
+        log.debug("request command: {}", command);
+
+        registry.register(command.text());
+    }
+
+    record CreateTodoCommand(String text) {
     }
 
 }
