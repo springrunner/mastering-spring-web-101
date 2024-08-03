c76ff1a48c3afbb44c1d6b4aab9ec5790a508b9d
Arawn Park <arawn.kr@gmail.com>
2024-08-03 21:56:40 +0900

    /api/todos/{id} 엔드포인트를 매핑하고 할일 수정 기능을 구현한다

--- src/main/java/todoapp/web/TodoRestController.java
+++ src/main/java/todoapp/web/TodoRestController.java
@@ -8,8 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import todoapp.core.todo.application.TodoFind;
+import todoapp.core.todo.application.TodoModification;
 import todoapp.core.todo.application.TodoRegistry;
 import todoapp.core.todo.domain.Todo;
+import todoapp.core.todo.domain.TodoId;
 
 import java.util.Objects;
 
@@ -19,12 +21,14 @@ public class TodoRestController {
 
     private final TodoFind find;
     private final TodoRegistry registry;
+    private final TodoModification modification;
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    public TodoRestController(TodoFind find, TodoRegistry registry) {
+    public TodoRestController(TodoFind find, TodoRegistry registry, TodoModification modification) {
         this.find = Objects.requireNonNull(find);
         this.registry = Objects.requireNonNull(registry);
+        this.modification = Objects.requireNonNull(modification);
     }
 
     @GetMapping
@@ -34,13 +38,20 @@ public class TodoRestController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void create(@RequestBody @Valid CreateTodoCommand command) {
+    public void create(@RequestBody @Valid TodoRestController.WriteTodoCommand command) {
         log.debug("request command: {}", command);
 
         registry.register(command.text());
     }
 
-    record CreateTodoCommand(@NotBlank @Size(min = 4, max = 140) String text) {
+    @PutMapping("/{id}")
+    public void update(@PathVariable("id") String id, @RequestBody @Valid WriteTodoCommand command) {
+        log.debug("request id: {}, command: {}", id, command);
+
+        modification.modify(TodoId.of(id), command.text, command.completed());
+    }
+
+    record WriteTodoCommand(@NotBlank @Size(min = 4, max = 140) String text, boolean completed) {
     }
 
 }
