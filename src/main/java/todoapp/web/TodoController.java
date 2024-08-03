4302f0990ac30d39e6558e1b659741fd537646e1
Arawn Park <arawn.kr@gmail.com>
2024-08-03 22:37:20 +0900

    /todos 엔드포인트를 매핑하고 CSV 형식으로 할일 다운로드 기능을 구현한다

--- src/main/java/todoapp/web/TodoController.java
+++ src/main/java/todoapp/web/TodoController.java
@@ -1,18 +1,31 @@
 package todoapp.web;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.AbstractView;
+import todoapp.core.todo.application.TodoFind;
+import todoapp.core.todo.domain.Todo;
 import todoapp.web.model.SiteProperties;
 
-import java.util.Objects;
+import java.util.*;
 
 @Controller
 public class TodoController {
 
+    private final TodoFind find;
     private final SiteProperties siteProperties;
 
-    public TodoController(SiteProperties siteProperties) {
+    public TodoController(TodoFind find, SiteProperties siteProperties) {
+        this.find = Objects.requireNonNull(find);
         this.siteProperties = Objects.requireNonNull(siteProperties);
     }
 
@@ -23,6 +36,48 @@ public class TodoController {
 
     @RequestMapping("/todos")
     public void todos() {
+
+    }
+
+    @RequestMapping(value = "/todos", produces = "text/csv")
+    public void downloadTodos(Model model) {
+        model.addAttribute("todos", find.all());
+    }
+
+    static class TodoCsvViewResolver implements ViewResolver {
+
+        @Override
+        public View resolveViewName(String viewName, Locale locale) throws Exception {
+            if ("todos".equals(viewName)) {
+                return new TodoCsvView();
+            }
+            return null;
+        }
+    }
+
+    static class TodoCsvView extends AbstractView implements View {
+
+        private final Logger log = LoggerFactory.getLogger(TodoController.class);
+
+        public TodoCsvView() {
+            setContentType("text/csv");
+        }
+
+        @Override
+        protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+            log.info("render model as csv content");
+
+            response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"todos.csv\"");
+            response.getWriter().println("id,text,completed");
+
+            var todos = (List<Todo>) model.getOrDefault("todos", Collections.emptyList());
+            for (var todo : todos) {
+                var line = "%s,%s,%s".formatted(todo.getId().toString(), todo.getText(), todo.isCompleted());
+                response.getWriter().println(line);
+            }
+
+            response.flushBuffer();
+        }
 
     }
 
--- src/main/java/todoapp/web/WebMvcConfiguration.java
+++ src/main/java/todoapp/web/WebMvcConfiguration.java
@@ -1,10 +1,17 @@
 package todoapp.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.ObjectToStringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import todoapp.core.todo.domain.Todo;
+
+import java.util.List;
 
 /**
  * Spring Web MVC 설정 정보이다.
@@ -24,8 +31,23 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(new TodoController.TodoCsvViewResolver());
+
         // registry.enableContentNegotiation();
         // 위와 같이 직접 설정하면, 스프링부트가 구성한 ContentNegotiatingViewResolver 전략이 무시된다.
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        var conversionService = new DefaultConversionService();
+        conversionService.addConverter(new Converter<Todo, String>() {
+            @Override
+            public String convert(Todo source) {
+                return source.toString();
+            }
+        });
+
+        converters.add(new ObjectToStringHttpMessageConverter(conversionService));
     }
 
     /**
