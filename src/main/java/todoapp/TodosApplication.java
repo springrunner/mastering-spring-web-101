0035399f5b8c0a7b1608d0a1a473cf58ff04a12e
Arawn Park <arawn.kr@gmail.com>
2024-08-03 17:39:04 +0900

    할일 목록 페이지에 사이트 속성을 추가한다

--- src/main/java/todoapp/TodosApplication.java
+++ src/main/java/todoapp/TodosApplication.java
@@ -2,12 +2,20 @@ package todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class TodosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodosApplication.class, args);
     }
+
+    // @ConfigurationPropertiesScan으로 자동 클래스 탐지로 빈 등록하기
+    // @Bean
+    // public SiteProperties siteProperties() {
+    //     return new SiteProperties();
+    // }
 
 }
--- src/main/java/todoapp/web/TodoController.java
+++ src/main/java/todoapp/web/TodoController.java
@@ -1,15 +1,36 @@
 package todoapp.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import todoapp.web.model.SiteProperties;
+
+import java.util.Objects;
 
 @Controller
 public class TodoController {
 
+    private final Environment environment;
+    private final String siteAuthor;
+    private final SiteProperties siteProperties;
+
+    public TodoController(Environment environment, @Value("${todoapp.site.author}") String siteAuthor, SiteProperties siteProperties) {
+        this.environment = Objects.requireNonNull(environment);
+        this.siteAuthor = Objects.requireNonNull(siteAuthor);
+        this.siteProperties = Objects.requireNonNull(siteProperties);
+    }
+
     @RequestMapping("/todos")
     public ModelAndView todos() {
+        // var siteProperties = new SiteProperties();
+        // siteProperties.setAuthor("Arawn Park");  
+        // siteProperties.setAuthor(environment.getProperty("todoapp.site.author"));  
+        // siteProperties.setAuthor(siteAuthor);
+
         var mav = new ModelAndView();
+        mav.addObject("site", siteProperties);
         mav.setViewName("todos");
 
         return mav;
