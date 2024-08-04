f591f50d6167926709aa9ed2a214e72386091dbb
Arawn Park <arawn.kr@gmail.com>
2024-08-04 15:36:28 +0900

    /api/user/profile 엔드포인트에 인가 프로세스를 추가한다

--- src/main/java/todoapp/web/UserRestController.java
+++ src/main/java/todoapp/web/UserRestController.java
@@ -1,23 +1,18 @@
 package todoapp.web;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import jakarta.annotation.security.RolesAllowed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import todoapp.security.UserSession;
 import todoapp.web.model.UserProfile;
 
-import java.util.Objects;
-
 @RestController
 public class UserRestController {
 
     @GetMapping("/api/user/profile")
-    public ResponseEntity<UserProfile> userProfile(UserSession userSession) {
-        if (Objects.nonNull(userSession)) {
-            return ResponseEntity.ok(new UserProfile(userSession.getUser()));
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    @RolesAllowed(UserSession.ROLE_USER)
+    public UserProfile userProfile(UserSession userSession) {
+        return new UserProfile(userSession.getUser());
     }
 
 }
