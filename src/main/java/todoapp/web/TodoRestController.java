16cd83bc3a785e54c345ac210c2448b6f9c57617
Arawn Park <arawn.kr@gmail.com>
2025-01-18 15:19:43 +0900

    할일 등록 기능에 유효성 검사를 추가한다

--- src/main/java/todoapp/web/TodoRestController.java
+++ src/main/java/todoapp/web/TodoRestController.java
@@ -1,5 +1,8 @@
 package todoapp.web;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -34,13 +37,13 @@ public class TodoRestController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void add(@RequestBody AddTodoCommand command) {
+    public void add(@RequestBody @Valid AddTodoCommand command) {
         log.debug("request command: {}", command);
 
         add.add(command.text());
     }
 
-    record AddTodoCommand(String text) {
+    record AddTodoCommand(@NotBlank @Size(min = 4, max = 140) String text) {
     }
 
 }
