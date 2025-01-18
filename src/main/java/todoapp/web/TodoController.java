c1287bb9bad92cfb6341f06035ee1c1da4101f3c
Arawn Park <arawn.kr@gmail.com>
2025-01-18 15:42:04 +0900

    /todos 엔드포인트를 매핑하고 CSV 형식으로 할일 다운로드 기능을 구현한다

--- src/main/java/todoapp/web/TodoController.java
+++ src/main/java/todoapp/web/TodoController.java
@@ -1,11 +1,21 @@
 package todoapp.web;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.AbstractView;
+import todoapp.core.todo.application.FindTodos;
+import todoapp.core.todo.domain.Todo;
 import todoapp.web.model.SiteProperties;
 
-import java.util.Objects;
+import java.util.*;
 
 
 /**
@@ -15,14 +25,62 @@ import java.util.Objects;
 public class TodoController {
 
     private final SiteProperties siteProperties;
+    private final FindTodos find;
 
-    public TodoController(SiteProperties siteProperties) {
+    public TodoController(SiteProperties siteProperties, FindTodos find) {
         this.siteProperties = Objects.requireNonNull(siteProperties);
+        this.find = Objects.requireNonNull(find);
     }
 
     @RequestMapping("/todos")
     public void todos(Model model) {
         model.addAttribute("site", siteProperties);
+    }
+
+    @RequestMapping(value = "/todos", produces = "text/csv")
+    public void downloadTodos(Model model) {
+        model.addAttribute("todos", find.all());
+    }
+
+    public static class TodoCsvViewResolver implements ViewResolver {
+        @Override
+        public View resolveViewName(String viewName, Locale locale) throws Exception {
+            if ("todos".equals(viewName)) {
+                return new TodoCsvView();
+            }
+            return null;
+        }
+    }
+
+    public static class TodoCsvView extends AbstractView implements View {
+
+        private final Logger log = LoggerFactory.getLogger(TodoController.class);
+
+        public TodoCsvView() {
+            setContentType("text/csv");
+        }
+
+        @Override
+        protected boolean generatesDownloadContent() {
+            return true;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+            log.info("render model as csv content");
+
+            response.addHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"todos.csv\"");
+            response.getWriter().println("id,text,completed");
+
+            var todos = (List<Todo>) model.getOrDefault("todos", Collections.emptyList());
+            for (var todo : todos) {
+                var line = "%s,%s,%s".formatted(todo.getId().toString(), todo.getText(), todo.isCompleted());
+                response.getWriter().println(line);
+            }
+
+            response.flushBuffer();
+        }
     }
 
 }
