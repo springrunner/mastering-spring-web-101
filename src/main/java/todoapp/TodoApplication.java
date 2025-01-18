f51e3a4e1529f040e479d1c7b2e6202650d1112c
Arawn Park <arawn.kr@gmail.com>
2025-01-18 14:31:07 +0900

    할일 목록 페이지에 사이트 속성을 추가한다

--- src/main/java/todoapp/TodoApplication.java
+++ src/main/java/todoapp/TodoApplication.java
@@ -2,11 +2,13 @@ package todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 /**
  * @author springrunner.kr@gmail.com
  */
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class TodoApplication {
 
     public static void main(String[] args) {
--- src/main/java/todoapp/web/model/SiteProperties.java
+++ src/main/java/todoapp/web/model/SiteProperties.java
@@ -1,10 +1,13 @@
 package todoapp.web.model;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 /**
  * 사이트 정보 모델
  *
  * @author springrunner.kr@gmail.com
  */
+@ConfigurationProperties("todoapp.site")
 public class SiteProperties {
 
     private String author = "unknown";
